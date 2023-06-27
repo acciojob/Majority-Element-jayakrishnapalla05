f00bcaba44da8f3f2e1126e7c9f84f17a09baa37
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-27 14:35:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+function MajorityElement(nums){
+	let map=new Map();
+	for(let i=0;i<nums.length;i++)
+		{
+			map.set(nums[i], (map.get(nums[i]  || 0))+1);
+		}
+
+	let maxcount=0;
+	let element;
+	for(let [num,count] of map)
+		{
+			if(count>maxcount)
+			{
+				maxcount=count;
+				element=num;
+			}
+		}
+	document.write("Majority Elements "+element);
+	
+}
+MajorityElement();
